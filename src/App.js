d189dccb0b942f1d45dffd0b6727219ab22597a4
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-11 13:47:48 -0600

    first log

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Register from "./components/pages/registration.js";
+import SignIn from "./components/pages/signIn.js";
+import Home from "./components/pages/Home.js";
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/" element={<Home/>} />
+
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,91 @@
+import "../../App.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BackgroundImage from '../../img/white.png'
+import user from '../../img/user.png'
+import { faReddit, faBitcoin, faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLaptopCode, faWallet, faCoins, faUser, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+
+function Home() {
+
+
+
+
+    return (
+        <header style={HeaderStyle}>
+            <body className="flex-col">
+                <div className="flex-col column">
+                    <div className="flex-row icon-wrapper">
+
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-bitcoin" icon={faBitcoin} />
+                        </Link>
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-reddit" icon={faReddit} />
+                        </Link>
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-laptop" icon={faLaptopCode} />
+                        </Link>
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-instagram" icon={faSquareInstagram} />
+                        </Link>
+                    </div>
+                    <div className="flex-column icon-wrapper-column">
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-wallet" icon={faWallet} />
+                        </Link>
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-coins" icon={faCoins} />
+                        </Link>
+                        <Link to="/register">
+                            <FontAwesomeIcon className="icon-bracket" icon={faRightToBracket} />
+                        </Link>
+                        <Link to="/">
+                            <FontAwesomeIcon className="icon-user" icon={faUser} />
+                        </Link>
+                    </div>
+                    <div className="flex-row user-icon-wrapper">
+                        <img className="user-icon-image" src={user} alt="user" />
+                    </div>
+                    <div className=" flex-column user-info-wrapper">
+                        <div className="user-info"><h1>View Wallet?</h1></div>
+                        <div className="user-info"><h2>View converter?</h2></div>
+                        <div className="user-info"><h3>View profile?</h3></div>
+                        <div className="user-info"><h4>login?</h4></div>
+                    </div>
+                </div>
+                <div className="section">
+                    
+                    <div className="flex-row header"></div>
+                </div>
+
+                
+
+                
+
+                <div className="spacer">
+
+                </div>
+
+            </body>
+
+
+
+
+
+
+
+        </header>
+    )
+}
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+
+
+}
+export default Home;
