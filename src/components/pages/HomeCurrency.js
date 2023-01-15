4d877677b31874933fac691a22625f0a5c1a7fba
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-15 10:48:19 -0600

    added crypto button colors

--- src/components/pages/HomeCurrency.js
+++ src/components/pages/HomeCurrency.js
@@ -4,19 +4,20 @@ import CryptoTitle from "../reusables/CryptoFile";
 
 function Crypto() {
     const titles = [
-        { id: 1, title: 'BTC', rate: 33834, icon: <i class="fa-brands fa-bitcoin"></i> },
-        { id: 2, title: 'ETH', rate: 1340, icon: <i class="fa-brands fa-ethereum"></i> },
-        { id: 3, title: 'CDR', rate: 0.27054, icon: <i class="fa-solid fa-code"></i> },
+        { id: 1, title: 'BTC', rate: 33834, icon: <i class="fa-brands fa-bitcoin" />},
+        { id: 2, title: 'ETH', rate: 1340, icon: <i class="fa-brands fa-ethereum" />},
+        { id: 3, title: 'CDR', rate: 0.27054, icon: <i class="fa-solid fa-code" />},
     ]
     return (
 
 
         <div className="container">
+
             <div className="row">
                 <div className="col-6">
                     <div className="d-flex">
                         {titles.map((item) => (
-                            <CryptoTitle  data={item}/>
+                            <CryptoTitle data={item} />
                         ))}
                     </div>
                 </div>
@@ -25,6 +26,7 @@ function Crypto() {
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/components/reusables/CryptoFile.jsx
+++ src/components/reusables/CryptoFile.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CryptoTitle = ({data}) => {
     const {id, title , rate , icon }= data
     return (
-        <div className="card">
+        <div className="card selected">
             <div className="card-body">
                 <div className="coin-icon mb-3">{icon}</div>
                     <div>{title}</div>
