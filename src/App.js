897958337006ef5491f9b52c2b8d130dda75cda0
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-17 11:44:46 -0600

    added new dashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
+// import Register from "./components/pages/registration.js";
+// import SignIn from "./components/pages/signIn.js";
+// import Home from "./components/pages/Home.js";
+// import Currency from "./components/pages/Currency.js";
+// import HomeCrypto from "./components/pages/HomeCurrency";
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/" element={<SignIn />} />
+//           <Route path="/Home" element={<Home/>} />
+//           <Route path="/currency" element={<Currency/>}/>
+//           <Route path="/crypto" element={<HomeCrypto/>} />
+
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect, useMemo } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Register from "./components/pages/registration.js";
 import SignIn from "./components/pages/signIn.js";
 import Home from "./components/pages/Home.js";
 import Currency from "./components/pages/Currency.js";
 import HomeCrypto from "./components/pages/HomeCurrency";
-
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<SignIn />} />
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/currency" element={<Currency/>}/>
-          <Route path="/crypto" element={<HomeCrypto/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+import { theme as customTheme } from './theme';
+import ColorModeContext from './components/ColorModeContext';
+import Layout from './layout/Layout';
+import Dashboard from './pages/Dashboard';
+const App = () => {
+  const [mode, setMode] = useState('dark');
+  const colorMode = useMemo(
+    () => ({
+      // The theme mode switch will invoke this method
+      toggleColorMode: () => {
+        window.localStorage.setItem('themeMode', mode === 'dark' ? 'light' : 'dark');
+        setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
+      },
+    }),
+    [mode],
   );
-}
+
+  useEffect(() => {
+    try {
+      const localTheme = window.localStorage.getItem('themeMode');
+      localTheme ? setMode(localTheme) : setMode('dark');
+    } catch {
+      setMode('dark');
+    }
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <Helmet
+        titleTemplate="%s | Timothy Currency Converter"
+        defaultTitle="Tim's Currency Converter"
+        
+      />
+
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={customTheme[mode]}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route exact path='/' element={<Dashboard />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/currency" element={<Currency />} />           <Route path="/crypto" element={<HomeCrypto />} />
+
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </HelmetProvider>
+  );
+};
 
 export default App;
