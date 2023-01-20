aac4c0a47efae9dd87b6e7073f702c80d11b65f7
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-20 08:14:04 -0600

    added cart

--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import Cart from '../../components/reusables/Cart'
 export default function cart() {
     return (
         <div>
-            
-
+            <header style={HeaderStyle}>
+            <Cart/>
+            </header>
         </div>
+        
     )
+}
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    // background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+
+
 }
--- src/components/reusables/Cart.js
+++ src/components/reusables/Cart.js
@@ -1,15 +1,71 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios'
+import '../../css/Cart.css'
 
 
 
 function Cart() {
+    const [cart, setCart] = useState({
+      currency: "",
+      amount: ""
+  
+     })
+  
+  
+     const changeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        const tempCart = { ...cart }
+        tempCart[name] = value
+        setCart(tempCart)
+     }
+  
+     const submitHandler = () => {
+  
+  
+  
+        axios.post("http://localhost:8081/cart/addToCart", cart)
+           .then((response) => {
+  
+  
+  
+              localStorage.setItem("Cart-Credentials", JSON.stringify(cart))
+  
+  
+              console.log(response.data)
+  
+              alert("Cart created successfully")
+           }).catch((e) => {
+              console.log(e)
+           })
+  
+     }
+    
+    
+    
+    
+    
+    
+    
+    
     return (
 
         
-<div>
-    
-</div>
+<div className="input-wrapper">
+         <div className="input-group">
+            
+            <div className="currency-input">
+               <input placeholder="Currency: " type="text" onChange={changeHandler} name="currency" value={cart.currency} />
+            </div>
+            <div className="amount-input">
+               <input placeholder="Amount: " type="text" onChange={changeHandler} name="amount" value={cart.amount} />
+            </div>
+            
+            <div className="btn-position">
+               <button className="btn-size" onClick={submitHandler}>Submit</button>
+            </div>
+         </div>
+      </div>
   
     )
 }
