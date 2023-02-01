482668e1f2e06f541b87f20229e0e113bf068282
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-02-01 10:26:06 -0600

    added style to side bar

--- src/Creator/CreatorBox.js
+++ src/Creator/CreatorBox.js
@@ -2,10 +2,22 @@ import React from 'react'
 import Sidebar from './SideBar/Side'
 function CreatorBox() {
   return (
+    <header style={HeaderStyle}>
+    
     <div>
     <Sidebar/>
     </div>
+    </header>
   )
+}
+const HeaderStyle = {
+  width: "100%",
+  height: "100vh",
+  // background: `url(${BackgroundImage})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundColor: "white"
 }
 
 export default CreatorBox
