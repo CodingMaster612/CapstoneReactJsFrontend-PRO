e112c5aae57cd1515f3ad0579dc72377c15f5066
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-02-02 13:14:37 -0600

    need to style iamge

--- src/Creator/AdminInfo/AdminInfoBox.js
+++ src/Creator/AdminInfo/AdminInfoBox.js
@@ -3,13 +3,22 @@ import '../AdminInfo/AdminInfo.css'
 function AdminInfoBox(props) {
     return (
         <div>
-            <div className="Admin-Info-Wrapper">
-                <div className="Bought-Style" >{props.cart.Bought_id}</div>
-                <div className="Img-Style"><img src={props.cart.image}></img></div>
-                <div className="Amount-Style"><h1 className="Amount-h1"> Amount: {props.cart.amount}</h1></div>
-                <div className="Currency-Style"><h2 className="Currency-h2">Currency:{props.cart.currency}</h2></div>
-            </div>
-        </div>
+            
+            
+
+            
+                    
+
+                        <div><img src={props.cart.image}></img></div>
+                        
+                        <div className="Amount-Style"><h1 className="Amount-h1"> Amount: {props.cart.amount}</h1></div>
+                        <div className="Currency-Style"><h2 className="Currency-h2">Currency: {props.cart.currency}</h2></div>
+                    </div>
+
+               
+
+
+          
     )
 }
 
--- src/Creator/CreatorBox.js
+++ src/Creator/CreatorBox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from './SideBar/Side'
 import Image from './AdminImage/Image'
+import BackgroundImage from '../img/white.png'
 import Rectangle from './Rectangle/Rectangle'
 function CreatorBox() {
   return (
@@ -16,7 +17,7 @@ function CreatorBox() {
 const HeaderStyle = {
   width: "100%",
   height: "100vh",
-  // background: `url(${BackgroundImage})`,
+  background: `url(${BackgroundImage})`,
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
