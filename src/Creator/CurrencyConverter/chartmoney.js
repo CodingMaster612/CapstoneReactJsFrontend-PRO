28d13147538a374e52b05d78171548bc1040d341
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-02-07 15:43:03 -0600

    added final graph

--- src/Creator/CurrencyConverter/chartmoney.js
+++ src/Creator/CurrencyConverter/chartmoney.js
@@ -1,55 +1,44 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../CurrencyConverter/CurrencyConverter.css'
+import { Bar } from 'react-chartjs-2';
 
-const CurrencyDoughnutChart = () => {
-  const [data, setData] = useState([]);
+const BarChart = () => {
+  const [data, setData] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get(
-        "https://api.exchangerate-api.com/v4/latest/USD"
-      );
-      setData(Object.entries(result.data.rates));
+      const result = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+
+      const chartData = {
+        labels: result.data.map(coin => coin.name),
+        datasets: [
+          {
+            label: 'Market Cap (USD)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
+            hoverBorderColor: 'rgba(255, 99, 132, 1)',
+            data: result.data.map(coin => coin.market_cap),
+          }
+        ]
+      };
+
+      setData(chartData);
     };
+
     fetchData();
   }, []);
 
-  const renderDoughnut = () => {
-    const canvas = document.getElementById("currency-doughnut-chart");
-    const ctx = canvas.getContext("2d");
-
-    const total = data.reduce((sum, item) => sum + item[1], 0);
-
-    let startAngle = 0;
-    data.forEach(item => {
-      const endAngle = startAngle + (item[1] / total) * 2 * Math.PI;
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2, canvas.height / 2);
-      ctx.arc(
-        canvas.width / 2,
-        canvas.height / 2,
-        canvas.width / 2,
-        startAngle,
-        endAngle
-      );
-      ctx.closePath();
-      ctx.fillStyle = "lightblue";
-      ctx.fill();
-      startAngle = endAngle;
-    });
-  };
-
-  useEffect(() => {
-    if (data.length > 0) {
-      renderDoughnut();
-    }
-  }, [data]);
-
   return (
-    <div>
-      <canvas id="currency-doughnut-chart" width="300" height="400"></canvas>
+    <div className="chart-size">
+      { data.labels ? <Bar data={data} /> : null }
+      <div className="text-position">
+        <h1 >PUT CONTENT HERE</h1>
+      </div>
     </div>
   );
 };
 
-export default CurrencyDoughnutChart;
+export default BarChart;
