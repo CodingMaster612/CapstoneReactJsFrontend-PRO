0fad8ae8589ed6b932e044edcbe41b4584d5fed3
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-02-07 10:09:34 -0600

    finihed

--- src/Creator/CurrencyConverter/CurrencyConverter.js
+++ src/Creator/CurrencyConverter/CurrencyConverter.js
@@ -7,17 +7,43 @@ const CurrencyConverter = () => {
   const [toCurrency, setToCurrency] = useState("EUR");
   const [convertedAmount, setConvertedAmount] = useState(0);
   const [exchangeRates, setExchangeRates] = useState({});
+  const [cart, setCart] = useState([])
 
+  
+  
+
+const submitHandler = () => {
+
+const convertedAmount = localStorage.getItem("stored-currency")
+
+    axios.post(`http://localhost:8081/cart/addMoneyToCart/${convertedAmount}`, cart)
+    .then((response) => {
+        console.log(response.data)
+        alert("you made it")
+
+       }).catch((e) => {
+          console.log(e)
+       })
+
+ }
+
+
+  
+  
   useEffect(() => {
     axios
       .get("https://api.exchangerate-api.com/v4/latest/USD")
       .then(response => {
         setExchangeRates(response.data.rates);
+        localStorage.setItem("stored-currency", convertedAmount)
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
+
+
+
 
   useEffect(() => {
     if (Object.keys(exchangeRates).length !== 0) {
@@ -61,6 +87,9 @@ const CurrencyConverter = () => {
       </form>
 
       <p>{amount} {fromCurrency} = {convertedAmount} {toCurrency}</p>
+      {/* <div>
+        <button onClick={submitHandler}  className="button-curr"/>
+      </div> */}
     </div>
   );
 };
